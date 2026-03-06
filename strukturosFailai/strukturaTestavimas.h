bba118f451c8f4513318a5c1d8aa6d369bbc3bda
IvarasJoma <aivarasjomantas@gmail.com>
2026-03-06 15:54:40 +0200

    Sutvarkytas header failas

--- strukturosFailai/strukturaTestavimas.h
+++ strukturosFailai/strukturaTestavimas.h
@@ -9,20 +9,16 @@ struct TestoLaikai {
     double skaiciavimas = 0.0;
     double rikiavimas = 0.0;
     double isvedimas = 0.0;
-    double bendras() const {
-        return nuskaitymas + skaiciavimas + rikiavimas + isvedimas;
-    }
-    void padalintiIs(int kiekis) {
-        nuskaitymas /= kiekis;
-        skaiciavimas /= kiekis;
-        rikiavimas /= kiekis;
-        isvedimas /= kiekis;
-    }
 };
 
-double apskaiciuotiLaika(std::chrono::steady_clock::time_point startas, std::chrono::steady_clock::time_point pabaiga);
+double bendras(const TestoLaikai& laikai);
+void padalintiIs(TestoLaikai& laikai, int kiekis);
 
-template <typename Func> double ismatuotiLaika(Func veiksmas) {
+double apskaiciuotiLaika(std::chrono::steady_clock::time_point startas,
+                         std::chrono::steady_clock::time_point pabaiga);
+
+template <typename Func>
+double ismatuotiLaika(Func veiksmas) {
     auto pradzia = std::chrono::steady_clock::now();
     veiksmas();
     auto pabaiga = std::chrono::steady_clock::now();
