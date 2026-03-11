992d2d34955a6167305f87578e584b4ee4446a4b
IvarasJoma <aivarasjomantas@gmail.com>
2026-03-11 16:45:01 +0200

    Pašalintas miręs kodas

--- strukturosFailai/Failai.h
+++ strukturosFailai/Failai.h
@@ -10,10 +10,6 @@ struct Failai{
     std::vector<std::string> vyrPavardes = nuskaitytiEilutesIVektoriu("tekstiniaiFailai/Lietuviskos_vyru_pavardes.txt");
     std::vector<std::string> motVardai = nuskaitytiEilutesIVektoriu("tekstiniaiFailai/Lietuviski_moteru_vardai.txt");
     std::vector<std::string> motPavardes = nuskaitytiEilutesIVektoriu("tekstiniaiFailai/Lietuviskos_moteru_pavardes.txt");
-    std::string studentai10000 = "tekstiniaiFailai/studentai10000.txt";
-    std::string studentai100000 = "tekstiniaiFailai/studentai100000.txt";
-    std::string studentai1000000 = "tekstiniaiFailai/studentai1000000.txt";
-    std::string kursiokai = "tekstiniaiFailai/kursiokai.txt";
 };
 
 #endif
