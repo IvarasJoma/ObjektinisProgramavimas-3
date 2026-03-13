5ef0a84b8ff7cbadfdf625484147453ad5bafd8b
IvarasJoma <aivarasjomantas@gmail.com>
2026-03-13 09:14:06 +0200

    Pakeista vardų ir pavardžių sąrašo direktorija

--- strukturosFailai/Failai.h
+++ strukturosFailai/Failai.h
@@ -6,10 +6,10 @@
 #include "../strukturosFailai/strukturaDarbasSuFailais.h"
 
 struct Failai{
-    std::vector<std::string> vyrVardai = nuskaitytiEilutesIVektoriu("tekstiniaiFailai/Lietuviski_vyru_vardai.txt");
-    std::vector<std::string> vyrPavardes = nuskaitytiEilutesIVektoriu("tekstiniaiFailai/Lietuviskos_vyru_pavardes.txt");
-    std::vector<std::string> motVardai = nuskaitytiEilutesIVektoriu("tekstiniaiFailai/Lietuviski_moteru_vardai.txt");
-    std::vector<std::string> motPavardes = nuskaitytiEilutesIVektoriu("tekstiniaiFailai/Lietuviskos_moteru_pavardes.txt");
+    std::vector<std::string> vyrVardai = nuskaitytiEilutesIVektoriu("VarduIrPavardziuSarasai/Lietuviski_vyru_vardai.txt");
+    std::vector<std::string> vyrPavardes = nuskaitytiEilutesIVektoriu("VarduIrPavardziuSarasai/Lietuviskos_vyru_pavardes.txt");
+    std::vector<std::string> motVardai = nuskaitytiEilutesIVektoriu("VarduIrPavardziuSarasai/Lietuviski_moteru_vardai.txt");
+    std::vector<std::string> motPavardes = nuskaitytiEilutesIVektoriu("VarduIrPavardziuSarasai/Lietuviskos_moteru_pavardes.txt");
 };
 
 #endif
