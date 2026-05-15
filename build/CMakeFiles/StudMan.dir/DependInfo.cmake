e35331c3269f1ca4e3b2c70332ceaeb19fcddb66
IvarasJoma <aivarasjomantas@gmail.com>
2026-05-15 20:52:16 +0300

    Atnaujintas Setup.exe failas

---
+++ build/CMakeFiles/StudMan.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/Studentas.cpp" "CMakeFiles/StudMan.dir/Studentas.cpp.o" "gcc" "CMakeFiles/StudMan.dir/Studentas.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/pagalbinesFunkcijos/Meniu.cpp" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/Meniu.cpp.o" "gcc" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/Meniu.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/pagalbinesFunkcijos/pagalbinesDarboSuFailaisFunkcijos.cpp" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesDarboSuFailaisFunkcijos.cpp.o" "gcc" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesDarboSuFailaisFunkcijos.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/pagalbinesFunkcijos/pagalbinesGeneravimoFunkcijos.cpp" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesGeneravimoFunkcijos.cpp.o" "gcc" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesGeneravimoFunkcijos.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/pagalbinesFunkcijos/pagalbinesIsvestiesFunkcijos.cpp" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesIsvestiesFunkcijos.cpp.o" "gcc" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesIsvestiesFunkcijos.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/pagalbinesFunkcijos/pagalbinesIvestiesFunkcijos.cpp" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesIvestiesFunkcijos.cpp.o" "gcc" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesIvestiesFunkcijos.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/pagalbinesFunkcijos/pagalbinesRikiavimoFunkcijos.cpp" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesRikiavimoFunkcijos.cpp.o" "gcc" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesRikiavimoFunkcijos.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/pagalbinesFunkcijos/pagalbinesSkaiciavimuFunkcijos.cpp" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesSkaiciavimuFunkcijos.cpp.o" "gcc" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesSkaiciavimuFunkcijos.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/pagalbinesFunkcijos/pagalbinesTestavimoFunkcijos.cpp" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesTestavimoFunkcijos.cpp.o" "gcc" "CMakeFiles/StudMan.dir/pagalbinesFunkcijos/pagalbinesTestavimoFunkcijos.cpp.o.d"
+  "/Users/jomaras06/Documents/GitHub/ObjektinisProgramavimas/programaVektoriai.cpp" "CMakeFiles/StudMan.dir/programaVektoriai.cpp.o" "gcc" "CMakeFiles/StudMan.dir/programaVektoriai.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
